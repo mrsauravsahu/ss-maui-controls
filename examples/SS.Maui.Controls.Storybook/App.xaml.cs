d7028ed3719bd50beda8a293e271bd0f5a3277e1
Saurav Sahu <mrsauravsahu@outlook.com>
2023-07-08 17:51:07 +0530

    chore: add an examples project to showcase and use the RatingInput control

---
+++ examples/SS.Maui.Controls.Storybook/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SS.Maui.Controls.Storybook;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ examples/SS.Maui.Controls.Storybook/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SS.Maui.Controls.Storybook;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
