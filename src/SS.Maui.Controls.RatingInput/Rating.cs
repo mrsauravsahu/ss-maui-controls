fd0b414d189b5d38e850f91a1097f8c73583924f
Saurav Sahu <mrsauravsahu@outlook.com>
2023-07-09 01:31:00 +0530

    feat: add rating input control

---
+++ src/SS.Maui.Controls.RatingInput/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SS.Maui.Controls.RatingInput
+{
+	public partial class Rating : ObservableObject
+	{
+		[ObservableProperty]
+		private int value;
+
+		[ObservableProperty]
+		private bool isRated;
+	}
+}
+
